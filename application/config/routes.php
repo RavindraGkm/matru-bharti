9e647205d14a354a0a29d86cd15a721557a56003
RavindraGkm <rajsinghravi25.87@gmail.com>
2016-01-30 13:12:08 +0530

    fixed session problem

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Index_controller';
 $route['404_override'] = '';
 $route['profile'] = "Profile_controller";
+$route['profile_info'] = "Profile_controller/profile_session";
+$route['profile_session_access'] = "Profile_controller/profile_session_access";
 $route['authors/([0-9]+)'] = "authors/index/$1";
 $route['translate_uri_dashes'] = FALSE;
--- application/controllers/Profile_controller.php
+++ application/controllers/Profile_controller.php
@@ -8,5 +8,23 @@ class Profile_controller extends CI_Controller {
         $this->load->helper('url');
         $this->load->view('profile/index',$data);
     }
+    public function profile_session() {
+        $this->load->library('session');
+        $remember_token = $this->input->post('token');
+        $author_id = $this->input->post('id');
+        $session_data = array(
+            'author_id'  => $author_id,
+            'remember_token'     => $remember_token,
+            'logged_in' => TRUE
+        );
+        $this->session->set_userdata($session_data);
 
+        echo json_encode(array('status'=>'success'));
+    }
+    public function profile_session_access() {
+        $this->load->library('session');
+        $this->session->unset_userdata('some_name');
+        $this->session->set_userdata('some_name', 'some_value');
+        echo json_encode(array('status'=>'success'));
+    }
 }
