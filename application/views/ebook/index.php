254657d819513c9ef916a338adfe643a5dab55fa
dileeplohar <dileeplohar@gmail.com>
2016-02-04 18:16:35 +0530

    ebook/index.php

--- application/views/ebook/index.php
+++ application/views/ebook/index.php
@@ -87,10 +87,10 @@
             <div class="card col col-md-offset-1 col-lg-10 col-md-10 col-sm-12 col-xs-12" id="profile-main">
                 <input type="hidden" value="<?php echo $active_tab; ?>" id="active_tab_val" />
                 <ul class="tab-nav" role="tablist">
-                    <li role="presentation" id="tab_ebook"><a href="#ebook" aria-controls="ebook" role="tab" data-toggle="tab">e-book Upload</a></li>
-                    <li role="presentation" id="tab_composition"><a href="#composition" aria-controls="composition" role="tab" data-toggle="tab">composition / Creation upload</a></li>
-                    <li role="presentation" id="tab_ebook_list"><a href="#ebook_list" aria-controls="upload-file-list" role="tab" data-toggle="tab">List of Upload Files</a></li>
-                    <li role="presentation" id="tab_composition_list"><a href="#composition_list" aria-controls="upload-file-list" role="tab" data-toggle="tab">List of Upload Files</a></li>
+                    <li role="presentation" id="tab_ebook"><a href="#ebook" aria-controls="ebook" role="tab" data-toggle="tab"><i class="fa fa-book"></i>&nbsp;e-book Upload</a></li>
+                    <li role="presentation" id="tab_composition"><a href="#composition" aria-controls="composition" role="tab" data-toggle="tab"><i class="fa fa-file-word-o"></i>&nbsp;composition / Creation upload</a></li>
+                    <li role="presentation" id="tab_ebook_list"><a href="#ebook_list" aria-controls="upload-file-list" role="tab" data-toggle="tab"><i class="fa fa-list"></i>&nbsp;List of E-book Files</a></li>
+                    <li role="presentation" id="tab_composition_list"><a href="#composition_list" aria-controls="upload-file-list" role="tab" data-toggle="tab"><i class="fa fa-list"></i>&nbsp;List of Composition Files</a></li>
                 </ul>
 
                 <!-- Tab panes -->
